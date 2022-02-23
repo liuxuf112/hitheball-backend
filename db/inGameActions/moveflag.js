d5b7f77fdd78f32c38e11628732e9682e77411ff
liuxuf112 <liuxufeng1017@gmail.com>
2022-02-22 20:52:33 -0800

    Update moveflag.js

--- db/inGameActions/moveflag.js
+++ db/inGameActions/moveflag.js
@@ -65,7 +65,7 @@ async function trytomoveflag(httpRequest,httpResponse,gameId,deviceId){
     console.log(attemptStealFlag.flag_id);
 
     try{
-        await queries.setflaglocation(attemptStealFlag.flag_id, tempX, tempY);
+        await queries.setflaglocation(attemptStealFlag.flag_id, 50, 150);
         
         console.log(stealFlagLocation.x);
         console.log(stealFlagLocation.y);
